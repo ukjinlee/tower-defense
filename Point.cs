bafe30ab8497189e88e29831b5acaf5cbcb83760
asllan <asllan77@gmail.com>
2017-11-08 11:05:18 -0500

    Create Point class

---
+++ Point.cs
@@ -0,0 +1,14 @@
+namespace TowerDefense
+{
+    class Point
+    {
+        public readonly int X;
+        public readonly int Y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }    
+}
