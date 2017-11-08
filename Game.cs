df81715a1e4c192bdb9a207ece35c8d3bb3949d6
asllan <asllan77@gmail.com>
2017-11-08 10:54:57 -0500

    Define Map class

--- Game.cs
+++ Game.cs
@@ -4,7 +4,8 @@ namespace TowerDefense
     {
         public static void Main()
         {
-            Tower tower = new Tower();
+            Map map = new Map(8, 5);
+            int area = map.Width * map.Height;
         }    
     }    
 }
--- Map.cs
+++ Map.cs
@@ -2,5 +2,13 @@ namespace TowerDefense
 {
     class Map
     {
+        public readonly int Width;
+        public readonly int Height;
+
+        public Map(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
     }    
 }
