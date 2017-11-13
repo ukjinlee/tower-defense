0e9bf91796ceec518b4141721a4448b78d290c80
asllan <asllan77@gmail.com>
2017-11-12 21:49:11 -0500

    Throw exception when map location is out of the map.

--- Game.cs
+++ Game.cs
@@ -6,13 +6,16 @@ namespace TowerDefense
     {
         public static void Main()
         {
-            Point m = new MapLocation(4, 2);
-            Console.WriteLine(m is Point);
-            Console.WriteLine(m is MapLocation);
+            Map map = new Map(8, 5);
 
-            Point p = new Point(5, 6);
-            Console.WriteLine(p is Point);
-            Console.WriteLine(p is MapLocation);
+            try
+            {
+                MapLocation location = new MapLocation(20, 20, map);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("That map location is not on the map!");
+            }
         }
     }    
 }
--- MapLocation.cs
+++ MapLocation.cs
@@ -4,8 +4,12 @@ namespace TowerDefense
 {
     class MapLocation: Point
     {
-        public MapLocation(int x, int y): base(x, y)
+        public MapLocation(int x, int y, Map map): base(x, y)
         {
+            if (!map.OnMap(this))
+            {
+                throw new Exception();
+            }
         }
     }    
 }
