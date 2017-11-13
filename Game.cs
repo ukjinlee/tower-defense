fca1aff4741011afdacf4884d7e93556affd7395
asllan <asllan77@gmail.com>
2017-11-13 17:28:52 -0500

    Make a test code about null exception

--- Game.cs
+++ Game.cs
@@ -10,16 +10,22 @@ namespace TowerDefense
 
             try
             {
-                MapLocation[] path = {
-                    new MapLocation(0, 2, map),
-                    new MapLocation(1, 2, map),
-                    new MapLocation(2, 2, map),
-                    new MapLocation(3, 2, map),
-                    new MapLocation(4, 2, map),
-                    new MapLocation(5, 2, map),
-                    new MapLocation(6, 2, map),
-                    new MapLocation(7, 2, map),
-                };
+                Path path = new Path(
+                    new [] {
+                        new MapLocation(0, 2, map),
+                        new MapLocation(1, 2, map),
+                        new MapLocation(2, 2, map),
+                        new MapLocation(3, 2, map),
+                        new MapLocation(4, 2, map),
+                        new MapLocation(5, 2, map),
+                        new MapLocation(6, 2, map),
+                        new MapLocation(7, 2, map),
+                });
+                MapLocation location = path.GetLocationAt(8);
+                if (location != null)
+                {
+                    Console.WriteLine($"{location.X}, {location.Y}");
+                }
             }
             catch (OutOfBoundsException ex)
             {
@@ -29,9 +35,9 @@ namespace TowerDefense
             {
                 Console.WriteLine("Unhandled TowerDefenseException");
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                Console.WriteLine("Unhandled Exception");
+                Console.WriteLine($"Unhandled Exception: {ex}");
             }
         }
     }    
