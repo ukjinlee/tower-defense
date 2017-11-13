85bea7e600b12db635904ee909b3b4f9897210fc
asllan <asllan77@gmail.com>
2017-11-13 16:15:24 -0500

    Create Path class

--- Game.cs
+++ Game.cs
@@ -10,7 +10,16 @@ namespace TowerDefense
 
             try
             {
-                MapLocation location = new MapLocation(20, 20, map);
+                MapLocation[] path = {
+                    new MapLocation(0, 2, map),
+                    new MapLocation(1, 2, map),
+                    new MapLocation(2, 2, map),
+                    new MapLocation(3, 2, map),
+                    new MapLocation(4, 2, map),
+                    new MapLocation(5, 2, map),
+                    new MapLocation(6, 2, map),
+                    new MapLocation(7, 2, map),
+                };
             }
             catch (OutOfBoundsException ex)
             {
--- Path.cs
+++ Path.cs
@@ -2,5 +2,11 @@ namespace TowerDefense
 {
     class Path
     {
+        private readonly MapLocation[] path;
+
+        public Path(MapLocation[] path)
+        {
+            this.path = path;
+        }
     }    
 }
