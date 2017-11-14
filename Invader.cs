034f76f36cf2e4a8539069fda71c58590b39dc0d
asllan <asllan77@gmail.com>
2017-11-14 14:59:28 -0500

    Make accessor methods for Invader class

--- Invader.cs
+++ Invader.cs
@@ -2,5 +2,16 @@ namespace TowerDefense
 {
     class Invader
     {
+        private MapLocation location;
+
+        public MapLocation GetLocation()
+        {
+            return location;
+        }
+
+        public void SetLocation(MapLocation value)
+        {
+            location = value;
+        }
     }    
 }
