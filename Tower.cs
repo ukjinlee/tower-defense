922110195846338f5cc189733f22388c2ffff570
asllan <asllan77@gmail.com>
2017-11-15 18:59:06 -0500

    Make constructor and FireOnInvaders method

--- Tower.cs
+++ Tower.cs
@@ -2,5 +2,19 @@ namespace TowerDefense
 {
     class Tower
     {
+        private readonly MapLocation location;
+
+        public Tower(MapLocation location)
+        {
+            this.location = location;
+        }
+
+        public void FireOnInvaders(Invader[] invaders)
+        {
+            foreach (var invader in invaders)
+            {
+                // Do stuff with invader
+            }
+        }
     }    
 }
