705a74c421e56be1a7a0b8772c51084da6a3b141
asllan <asllan77@gmail.com>
2017-11-14 20:36:14 -0500

    Add properties; HasScored, IsNeutralized, IsActive

--- Invader.cs
+++ Invader.cs
@@ -6,28 +6,18 @@ namespace TowerDefense
         private int pathStep = 0;
         
         public int Health { get; private set; } = 2;
-
-        public MapLocation Location 
-        { 
-            get
-            {
-                return path.GetLocationAt(pathStep);
-            }
-        }
+        public MapLocation Location => path.GetLocationAt(pathStep);
+        
+        public bool HasScored => pathStep >= path.Length;
+        public bool IsNeutralized => Health <= 0;
+        public bool IsActive => !(IsNeutralized || HasScored);
 
         public Invader(Path path)
         {
             this.path = path;
         }
 
-        public void Move()
-        {
-            pathStep++;
-        }
-
-        public void DecreaseHealth(int factor)
-        {
-            Health -= factor;
-        }
+        public void Move() => pathStep++;
+        public void DecreaseHealth(int factor) => Health -= factor;
     }    
 }
