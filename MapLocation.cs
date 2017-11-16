3f5eeb13ad5e86a15547ceed9cd5476f847ee9cd
asllan <asllan77@gmail.com>
2017-11-15 19:16:46 -0500

    Add InRangeOf method

--- MapLocation.cs
+++ MapLocation.cs
@@ -11,5 +11,10 @@ namespace TowerDefense
                 throw new OutOfBoundsException($"{x}, {y} is outside the boundaries of the map.");
             }
         }
+
+        public bool InRangeOf(MapLocation location, int range)
+        {
+            return DistanceTo(location) <= range;
+        }
     }    
 }
